9c5275df1b2f7fd3893d5322a583f6b8b7b52bae
srirampujari <pujarisriram61@gmail.com>
2026-03-11 10:22:07 +0530

    fsyt

---
+++ practice.js
@@ -0,0 +1,9 @@
+function leooo(a,b) {
+   return a+b;
+          
+
+} 
+let ram=leooo(2,8);
+let ram1=leooo(5,8);
+console.log(ram) ;
+console.log(ram1)                                                                                       
