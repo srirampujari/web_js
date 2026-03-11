f85b0a9bc2ccc6f03ee51a46d4b94a70bab7d02b
srirampujari <pujarisriram61@gmail.com>
2026-03-11 10:29:33 +0530

    Delete practice.js

--- practice.js
+++
@@ -1,9 +0,0 @@
-function leooo(a,b) {
-   return a+b;
-          
-
-} 
-let ram=leooo(2,8);
-let ram1=leooo(5,8);
-console.log(ram) ;
-console.log(ram1)                                                                                       
